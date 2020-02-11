02f9ff3078868374b9aa76548f42dd5ffe8cd5af
Jan-Matthis <mail@jan-matthis.de>
2020-02-11 23:13:43 +0100

    Use logsumexp from scipy.special

--- snl/inference/abc.py
+++ snl/inference/abc.py
@@ -4,9 +4,9 @@ Implementations of ABC algorithms for likelihood-free simulation-based inference
 
 import os
 import sys
-import numpy as np
-import scipy.misc
 
+import numpy as np
+import scipy.special
 import snl.util as util
 
 
@@ -16,7 +16,7 @@ def calc_dist(data_1, data_2):
     """
 
     if data_1 is None or data_2 is None:
-        return float('inf')
+        return float("inf")
 
     diff = data_1 - data_2
     dist = np.sqrt(np.dot(diff, diff))
@@ -34,14 +34,16 @@ class Rejection:
         self.prior = prior
         self.sim_model = sim_model
 
-    def run(self, obs_data, eps, n_samples, logger=sys.stdout, info=False, rng=np.random):
+    def run(
+        self, obs_data, eps, n_samples, logger=sys.stdout, info=False, rng=np.random
+    ):
 
         ps = []
         dist = []
         n_sims = 0
         n_accepted = 0
 
-        logger = open(os.devnull, 'w') if logger is None else logger
+        logger = open(os.devnull, "w") if logger is None else logger
 
         while n_accepted < n_samples:
 
@@ -57,7 +59,11 @@ class Rejection:
             if info:
                 dist.append(prop_dist)
 
-            logger.write('sim = {0}, accepted = {1}, dist = {2:.3}, acc rate = {3:.2%}\n'.format(n_sims, n_accepted, prop_dist, float(n_accepted) / n_sims))
+            logger.write(
+                "sim = {0}, accepted = {1}, dist = {2:.3}, acc rate = {3:.2%}\n".format(
+                    n_sims, n_accepted, prop_dist, float(n_accepted) / n_sims
+                )
+            )
 
         if info:
             return np.array(ps), np.array(dist), n_sims
@@ -77,7 +83,16 @@ class MCMC:
         self.cur_ps = np.asarray(init_ps, float)
         self.cur_log_prior = self.prior.eval(self.cur_ps, log=True)
 
-    def run(self, obs_data, eps, step, n_samples, logger=sys.stdout, info=False, rng=np.random):
+    def run(
+        self,
+        obs_data,
+        eps,
+        step,
+        n_samples,
+        logger=sys.stdout,
+        info=False,
+        rng=np.random,
+    ):
         """
         Runs mcmc abc. Uses a spherical gaussian proposal.
         """
@@ -87,7 +102,7 @@ class MCMC:
         cur_dist = None
         n_dim = self.cur_ps.size
 
-        logger = open(os.devnull, 'w') if logger is None else logger
+        logger = open(os.devnull, "w") if logger is None else logger
 
         for i in range(n_samples):
 
@@ -109,7 +124,11 @@ class MCMC:
 
             ps.append(self.cur_ps.copy())
 
-            logger.write('iter = {0}, dist = {1:.3}, acc rate = {2:.2%}\n'.format(i, cur_dist, float(n_accepted) / (i+1)))
+            logger.write(
+                "iter = {0}, dist = {1:.3}, acc rate = {2:.2%}\n".format(
+                    i, cur_dist, float(n_accepted) / (i + 1)
+                )
+            )
 
         ps = np.array(ps)
         acc_rate = float(n_accepted) / n_samples
@@ -130,7 +149,18 @@ class SMC:
         self.prior = prior
         self.sim_model = sim_model
 
-    def run(self, obs_data, eps_init, eps_last, eps_decay, n_particles, ess_min=0.5, logger=sys.stdout, info=False, rng=np.random):
+    def run(
+        self,
+        obs_data,
+        eps_init,
+        eps_last,
+        eps_decay,
+        n_particles,
+        ess_min=0.5,
+        logger=sys.stdout,
+        info=False,
+        rng=np.random,
+    ):
         """
         Runs full smc abc.
         """
@@ -141,7 +171,7 @@ class SMC:
         all_log_ess = []
         all_n_sims = []
 
-        logger = open(os.devnull, 'w') if logger is None else logger
+        logger = open(os.devnull, "w") if logger is None else logger
 
         # save some log values for reuse
         log_ess_min = np.log(ess_min)
@@ -150,7 +180,9 @@ class SMC:
         # sample initial population
         iter = 0
         eps = eps_init
-        ps, n_sims = self.sample_initial_population(obs_data, n_particles, eps, logger, rng)
+        ps, n_sims = self.sample_initial_population(
+            obs_data, n_particles, eps, logger, rng
+        )
         log_weights = np.full(n_particles, -log_n_particles)
 
         if info:
@@ -160,18 +192,24 @@ class SMC:
             all_log_ess.append(0.0)
             all_n_sims.append(n_sims)
 
-        logger.write('iter = {0}, eps = {1}, ess (%) = {2}, sims = {3}\n'.format(iter, eps, 1.0, n_sims))
+        logger.write(
+            "iter = {0}, eps = {1}, ess (%) = {2}, sims = {3}\n".format(
+                iter, eps, 1.0, n_sims
+            )
+        )
 
         while eps > eps_last:
 
             # sample next population
             iter += 1
             eps *= eps_decay
-            ps, log_weights, n_new_sims = self.sample_next_population(ps, log_weights, obs_data, eps, logger, rng)
+            ps, log_weights, n_new_sims = self.sample_next_population(
+                ps, log_weights, obs_data, eps, logger, rng
+            )
             n_sims += n_new_sims
 
             # calculate effective sample size
-            log_ess = -scipy.misc.logsumexp(2.0 * log_weights) - log_n_particles
+            log_ess = -scipy.special.logsumexp(2.0 * log_weights) - log_n_particles
 
             # if population is degenerate, resample particles
             if log_ess < log_ess_min:
@@ -185,7 +223,11 @@ class SMC:
                 all_log_ess.append(log_ess)
                 all_n_sims.append(n_sims)
 
-            logger.write('iter = {0}, eps = {1}, ess (%) = {2}, sims = {3}\n'.format(iter, eps, np.exp(log_ess), n_sims))
+            logger.write(
+                "iter = {0}, eps = {1}, ess (%) = {2}, sims = {3}\n".format(
+                    iter, eps, np.exp(log_ess), n_sims
+                )
+            )
 
         if info:
             return all_ps, all_log_weights, all_eps, all_log_ess, all_n_sims
@@ -202,7 +244,7 @@ class SMC:
 
         for i in range(n_particles):
 
-            dist = float('inf')
+            dist = float("inf")
             prop_ps = None
 
             while dist > eps:
@@ -213,7 +255,7 @@ class SMC:
 
             ps.append(prop_ps)
 
-            logger.write('particle {0}\n'.format(i + 1))
+            logger.write("particle {0}\n".format(i + 1))
 
         return np.array(ps), n_sims
 
@@ -236,7 +278,7 @@ class SMC:
 
         for i in range(n_particles):
 
-            dist = float('inf')
+            dist = float("inf")
 
             while dist > eps:
                 idx = util.math.discrete_sample(weights, rng=rng)
@@ -246,13 +288,18 @@ class SMC:
                 n_sims += 1
 
             # calculate unnormalized weights
-            log_kernel = -0.5 * np.sum(scipy.linalg.solve_triangular(std, (new_ps[i] - ps).T, lower=True) ** 2, axis=0)
-            new_log_weights[i] = self.prior.eval(new_ps[i], log=True) - scipy.misc.logsumexp(log_weights + log_kernel)
+            log_kernel = -0.5 * np.sum(
+                scipy.linalg.solve_triangular(std, (new_ps[i] - ps).T, lower=True) ** 2,
+                axis=0,
+            )
+            new_log_weights[i] = self.prior.eval(
+                new_ps[i], log=True
+            ) - scipy.special.logsumexp(log_weights + log_kernel)
 
-            logger.write('particle {0}\n'.format(i + 1))
+            logger.write("particle {0}\n".format(i + 1))
 
         # normalize weights
-        new_log_weights -= scipy.misc.logsumexp(new_log_weights)
+        new_log_weights -= scipy.special.logsumexp(new_log_weights)
 
         return new_ps, new_log_weights, n_sims
 
